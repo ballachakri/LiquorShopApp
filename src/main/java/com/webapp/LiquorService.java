b809c9ea5b6a7d047dc6d2a704b29dd28b802571
chakravarthy <ballachakri@yahoo.com>
2019-10-14 12:00:01 +0100

    web application

---
+++ src/main/java/com/webapp/LiquorService.java
@@ -0,0 +1,31 @@
+package com.webapp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Chakra on 13/10/19.
+ */
+public class LiquorService {
+
+    public List getAvailableBrands(LiquorType type){
+
+        List brands = new ArrayList();
+
+        if(type.equals(LiquorType.WINE)){
+            brands.add("Adrianna Vineyard");
+            brands.add(("J. P. Chenet"));
+
+        }else if(type.equals(LiquorType.WHISKY)){
+            brands.add("Glenfiddich");
+            brands.add("Johnnie Walker");
+
+        }else if(type.equals(LiquorType.BEER)){
+            brands.add("Corona");
+
+        }else {
+            brands.add("No Brand Available");
+        }
+        return brands;
+    }
+}
---
+++ src/main/java/com/webapp/LiquorType.java
@@ -0,0 +1,9 @@
+package com.webapp;
+
+/**
+ * Created by Chakra on 13/10/19.
+ */
+public enum LiquorType {
+    WINE,BEER,WHISKY
+
+}
